0031d974e7cf971c04c658783be21b3c79ec33e2
Skitxoe/Skit <tbasebc@gmail.com>
2026-03-12 21:56:09 -0400

    Update version.js

--- version.js
+++ version.js
@@ -1,10 +1,7 @@
-// EchoDupe Version Control
 const CONFIG = {
-    version: "v1.0.1-BETA", 
-    buildName: "Build_Registry"
+    version: "v1.0.1-BETA"
 };
 
-// This function finds the version tag and updates the text
 document.addEventListener("DOMContentLoaded", () => {
     const versionElement = document.getElementById("app-version");
     if (versionElement) {
